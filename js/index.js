bd7f101dacbe6d5f6dc84211f2731dd4068b85f7
Nabeelah <nabeelahyousuph@gmail.com>
2019-05-14 13:07:32 +0100

    Add mouseover event listener to nav bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+let nav = document.querySelector('nav');
+
+
+// MOUSE OVER
+nav.addEventListener("mouseover", (e) => {
+    e.target.style.color = '#17A2B8';
+    console.log('mouse')
+    }
+)
+
